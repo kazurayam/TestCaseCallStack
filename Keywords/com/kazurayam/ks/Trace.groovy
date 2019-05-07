3652e5db964ee31711e20dd13b0272e2b14e82f2
kazurayam <kazuaki.urayama@gmail.com>
2019-05-08 07:21:50 +0900

    fist commit

---
+++ Keywords/com/kazurayam/ks/Trace.groovy
@@ -0,0 +1,66 @@
+package com.kazurayam.ks
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+
+import internal.GlobalVariable
+
+import MobileBuiltInKeywords as Mobile
+import WSBuiltInKeywords as WS
+import WebUiBuiltInKeywords as WebUI
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+
+class Trace {
+
+	@Keyword
+	def callTestCase(String testCaseId, Map parameters) {
+		List<String> testCaseCallStack = (List<String>)GlobalVariable.TestCaseCallStack
+		testCaseCallStack.add(testCaseId)
+		//
+		WebUI.callTestCase(findTestCase(testCaseId), parameters)
+		//
+		int last = testCaseCallStack.size()
+		String s = testCaseCallStack.remove(last)
+	}
+
+	@Keyword
+	def printCurrentTC() {
+		List<String> testCaseCallStack = (List<String>)GlobalVariable.TestCaseCallStack
+		int last = testCaseCallStack.size()
+		if (last > 0) {
+			println "TestCaseCallStack[${index}]=\"${testCaseCallStack.get(last)}\""
+		}
+	}
+}
